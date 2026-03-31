758210891bb38b3434dbb7f674da3151ca54c6bd
adamsophian <adam.sophianzzz@gmail.com>
2026-03-31 13:02:46 +0800

    finalized version of CLI
    - app doesnt quit when number is out of range (hopefully)
    - cleaned up the code
    - added comments

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import os
 from playsound3 import playsound
+sounds=os.listdir(r".\Sounds")
 
-def soundList():
+def soundList(): # function to print lists of sounds detected in "".\Sounds"
     listNum=(0)
     print("Available sounds:")
-    for file in os.listdir(r"C:\Users\Adam S\SynologyDrive\03 Personal\02 Projects\random ass soundboard\Sounds"):
+    for file in sounds:
         if file.endswith(".mp3"):
             listNum=(listNum+1)
             print(listNum, ":", file)
     return listNum 
 
-total=soundList()
 list=[]
-for file in os.listdir(r"C:\Users\Adam S\SynologyDrive\03 Personal\02 Projects\random ass soundboard\Sounds"):
+for file in sounds:
         if file.endswith(".mp3"):
             list.append(file)
-print(list)
 
-while True:
-    print("Play sound 1 -", total, "(0 to quit):")
+while True: # the main code
+    soundTotal=soundList()
+    print("Play sound 1 -", soundTotal, "(0 to quit):")
     option=int(input())
-    if not option == 0:
+    if option == 0:
+        print("Thank you for using SASS!")
+        break
+    elif option > soundTotal or option < 1:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Option", option, "does not exist. Try another option.")
+    else:
         os.system('cls' if os.name == 'nt' else 'clear')
         soundOption=list[option-1]
         print("Playing ",soundOption)
-        playsound(f".\Sounds\{soundOption}")
+        playsound(f".\\Sounds\\{soundOption}")
         os.system('cls' if os.name == 'nt' else 'clear')
-        soundList()
-    else:
-        break
