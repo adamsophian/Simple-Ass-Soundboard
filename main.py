3cd72199514b3c79ef4969fe91e6098a91444f6e
adamsophian <adam.sophianzzz@gmail.com>
2026-04-01 18:06:50 +0800

    Added checks if 'Sound' folder is empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from playsound3 import playsound
+import readchar
 soundDir=f"{os.path.abspath(os.path.dirname(__file__))}\\Sounds"
 sounds=os.listdir(soundDir)
 
@@ -14,23 +15,30 @@ def soundList(): # function to print lists of sounds detected in "".\Sounds"
 
 list=[]
 for file in sounds:
-        if file.endswith(".mp3") or file.endswith(".wav"):
-            list.append(file)
+    if file.endswith(".mp3") or file.endswith(".wav"):
+        list.append(file)
 
 os.system('cls' if os.name == 'nt' else 'clear')
-while True: # the main code
-    soundTotal=soundList()
-    print("Play sound 1 -", soundTotal, "(0 to quit):")
-    option=int(input())
-    if option == 0:
-        print("Thank you for using SASS!")
-        break
-    elif option > soundTotal or option < 1:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("Option", option, "does not exist. Try another option.")
-    else:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        soundOption=list[option-1]
-        print("Playing ",soundOption)
-        playsound(f"{soundDir}\\{soundOption}")
-        os.system('cls' if os.name == 'nt' else 'clear')
+
+if not list: # cheks if sounds folder is empty
+    print("'Sounds' folder is currently empty! Add audio files to continue")
+    print("Press Any Key To Exit")
+    readchar.readchar()
+
+else:
+    while True: # the main code
+        soundTotal=soundList()
+        print("Play sound 1 -", soundTotal, "(0 to quit):")
+        option=int(input())
+        if option == 0:
+            print("Thank you for using SASS!")
+            break
+        elif option > soundTotal or option < 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Option", option, "does not exist. Try another option.")
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            soundOption=list[option-1]
+            print("Playing ",soundOption)
+            playsound(f"{soundDir}\\{soundOption}")
+            os.system('cls' if os.name == 'nt' else 'clear')
